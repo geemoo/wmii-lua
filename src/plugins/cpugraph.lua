bddb907c94bac8fb1be5e8874f436442344e94fb
Jean Richard <jean@geemoo.ca>
2010-03-14 14:52:32 -0400

    cleaned up the code a bit and added a bunch of comments too

--- src/plugins/cpugraph.lua
+++ src/plugins/cpugraph.lua
@@ -38,11 +38,11 @@ local wmii = require("wmii")
 local os = require("os")
 local posix = require("posix")
 local io = require("io")
+local string = require("string")
 local type = type
 local error = error
 local pairs = pairs
 local tostring = tostring
-local string = string
 
 module("cpugraph")
 api_version = 0.1
@@ -54,8 +54,8 @@ local timer  = nil
 
 widget = wmii.widget:new ("400_cpugraph")
 
--- global var.. do these exist?
-local history = { }
+-- global var
+history = { }
 
 -- ------------------------------------------------------------
 -- looks into /sys/devices/system/cpu and gets a list of all 
@@ -76,33 +76,42 @@ local function cpu_list()
 	return list
 end
 
-
-function read_file(path)
-	local fd = io.open(path, "r")
-	if fd == nil then
-		return nil
+-- ------------------------------------------------------------------
+-- read a file, return the first line
+local function read_sys_line(file, fmt)
+	local ret = nil
+	local fd = io.open(file)
+	if fd then
+		ret = fd:read(fmt or "*l")
 	end
-
-	local text = fd:read("*a")
-	fd:close()
-
-	if type(text) == 'string' then
-		text = text:match('(%w+)')
-	end
-
-	return text
+	return ret
 end
 
 
+-- ------------------------------------------------------------------
+-- read a file and return the first line as a number
+local function read_sys_number(file)
+	local ret = read_sys_line(file, "*n")
+	if type(ret) ~= type(1) then
+		ret = 0
+	end
+	return ret
+end
+
+
+-- ------------------------------------------------------------------
+-- create a string describing the cpu speed for the last 10 intervals
 local function create_string(cpu)
 
 	-- read all the info we need and define other vars
-	local curfreq = (read_file(cpu .. '/cpufreq/scaling_cur_freq') / 1) or 0
-	local minfreq = (read_file(cpu .. '/cpufreq/scaling_min_freq') / 1) or 0
-	local maxfreq = (read_file(cpu .. '/cpufreq/scaling_max_freq') / 1) or 0
-	local increment = (maxfreq - minfreq) / 3
+	local curfreq = read_sys_number(cpu .. '/cpufreq/scaling_cur_freq')
+	local minfreq = read_sys_number(cpu .. '/cpufreq/scaling_min_freq')
+	local maxfreq = read_sys_number(cpu .. '/cpufreq/scaling_max_freq')
 	local freqsym = " "
 	local cpuname = string.gsub(cpu, ".*/", "")
+
+	-- figure out how big each frequency division is
+	local increment = (maxfreq - minfreq) / 3
 
 	-- figure out what symbol to use.. we split the bar into 3
 	if (curfreq <= minfreq + increment) then
@@ -110,7 +119,7 @@ local function create_string(cpu)
 	elseif (curfreq <= (minfreq + 2*increment)) then
 		freqsym = "o"
 	else 
-		freqsym = 'O'
+		freqsym = "O"
 	end
 
 	-- now shift over the history if needed and glue on this char
@@ -122,6 +131,8 @@ local function create_string(cpu)
 end
 
 
+-- ------------------------------------------------------------------
+-- update our plugin's display widgets
 function update ()
 	local txt = ""
 	local _, cpu
@@ -135,12 +146,17 @@ function update ()
 	widget:show(txt)
 end
 
-local function cpu_timer ( timer )
+
+-- ------------------------------------------------------------------
+-- schedule updates of our plugin
+local function cpugraph_timer ( timer )
 	update()
 	return 3
 end
 
 
+-- ------------------------------------------------------------------
+-- initialize our cpu speed history var for each cpu in system
 local function init_history ()
 	local txt = ""
 	local _, cpu
@@ -152,8 +168,10 @@ local function init_history ()
 	end
 end
 
+-- ------------------------------------------------------------------
+
 -- initialize the values in our history var for all the cpus we have
 init_history()
 
 -- now setup our timer to call the update function
-timer = wmii.timer:new (cpu_timer, 3)
+timer = wmii.timer:new (cpugraph_timer, 2)
